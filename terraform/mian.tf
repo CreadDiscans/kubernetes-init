ea7b48eb5a5ae5d65f10cca521b4f8197423b211
creaddiscans <creaddiscans@gmail.com>
2023-08-28 20:53:33 +0900

    변수 variable 처리

--- terraform/mian.tf
+++ terraform/mian.tf
@@ -1,18 +1,18 @@
 module "nginx_ingress" {
   source       = "./nginx_ingress"
-  external_ips = local.external_ips
-  email        = local.email
+  external_ips = var.external_ips
+  email        = var.email
 }
 
 module "nfs_provisioner" {
   source   = "./nfs_provisioner"
-  nfs_ip   = local.nfs_ip
-  nfs_path = local.nfs_path
+  nfs_ip   = var.nfs_ip
+  nfs_path = var.nfs_path
 }
 
 module "gitlab_devops" {
   source        = "./gitlab_devops"
-  domain        = local.domain
-  root_password = local.gitlab_root_password
+  domain        = var.domain
+  root_password = var.gitlab_root_password
   depends_on    = [module.nginx_ingress]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,29 @@
-locals {
-  external_ips         = "x.x.x.x-x.x.x.x"
-  email                = ""
-  domain               = ""
-  nfs_ip               = ""
-  nfs_path             = ""
-  gitlab_root_password = "" # 8자 이상
+variable "external_ips" {
+  type    = string
+  default = "x.x.x.x-x.x.x.x"
+}
+
+variable "email" {
+  type    = string
+  default = "user@example.com"
+}
+
+variable "domain" {
+  type    = string
+  default = "example.com"
+}
+
+variable "nfs_ip" {
+  type    = string
+  default = "x.x.x.x"
+}
+
+variable "nfs_path" {
+  type    = string
+  default = "/nfs"
+}
+
+variable "gitlab_root_password" {
+  type    = string
+  default = "defaultpassword"
 }
