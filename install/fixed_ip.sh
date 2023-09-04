d04544105305ca89f01e18da4f091462d9b8de89
creaddiscans <creaddiscans@gmail.com>
2023-09-04 13:23:34 +0000

    fixed ip

--- install/fixed_ip.sh
+++ install/fixed_ip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 IP_ADDRESS"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 IP_ADDRESS GATEWAY_IP" 
     exit -1
 fi
-sudo sed -i 's/dhcp4: true/dhcp4: false\n      addresses: [192.168.1.100\/24]\n      routes:\n        - to: default\n          via: 192.168.1.1\n      nameservers:\n        addresses: [8.8.8.8, 8.8.4.4]/g' /etc/netplan/00-installer-config.yaml
+sudo sed -i 's/dhcp4: true/dhcp4: false\n      addresses: ['$1'\/24]\n      routes:\n        - to: default\n          via: '$2'\n      nameservers:\n        addresses: [8.8.8.8, 8.8.4.4]/g' /etc/netplan/00-installer-config.yaml
 sudo netplan apply
