df2b37411147269a06e2c620867fae6a3b2e8ca5
creaddiscans <creaddiscans@gmail.com>
2023-09-04 13:20:12 +0000

    fixed ip

--- install/Readme.md
+++ install/Readme.md
@@ -3,10 +3,12 @@
 마스터 노드에서
 bash setup.sh
 bash fixed_ip.sh IP
+reboot
 bash master.sh
 
 워커 노드에서
 bash setup.sh
 bash fixed_ip.sh IP
+reboot
 bash wol.sh DEVICE_NAME # ifconfig로 확인
 bash worker.sh MASTER_IP TOKEN DISCOVERY_TOKEN_CA_CERT_HASH
--- install/fixed_ip.sh
+++ install/fixed_ip.sh
@@ -3,5 +3,5 @@ if [ $# -ne 1 ]; then
     echo "Usage: $0 IP_ADDRESS"
     exit -1
 fi
-sudo sed -i 's/dhcp4: true/ addresses:\n        - '$1'\/24/g' /etc/netplan/00-installer-config.yaml
+sudo sed -i 's/dhcp4: true/dhcp4: false\n      addresses: [192.168.1.100\/24]\n      routes:\n        - to: default\n          via: 192.168.1.1\n      nameservers:\n        addresses: [8.8.8.8, 8.8.4.4]/g' /etc/netplan/00-installer-config.yaml
 sudo netplan apply
