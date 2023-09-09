c0146743e2829349bf847931548ca8d5b7696893
creaddiscans <creaddiscans@gmail.com>
2023-09-09 15:21:17 +0000

    node ssh  환경 구성

--- install/master.sh
+++ install/master.sh
@@ -11,3 +11,9 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
 kubectl create secret generic kubeconfig --from-file=$HOME/.kube/config -n kube-system
+
+mkdir -p $HOME/.ssh
+ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/id_rsa -q -N ""
+kubectl create secret generic node-private --from-file=$HOME/.ssh/id_rsa -n kube-system
+kubectl create secret generic node-public --from-file=$HOME/.ssh/id_rsa.pub -n kube-system
+kubectl create secret generic node-username --from-literal=username=$USER -n kube-system
---
+++ install/register.sh
@@ -0,0 +1,66 @@
+node=$1
+if [ "$node" = "" ]; then
+    echo "Node name must be specified."
+    exit -1
+fi
+
+ns=kube-system
+pod=$(
+    kubectl create -n $ns -f - <<EOF
+apiVersion: v1
+kind: Pod
+metadata:
+  generateName: register-node-
+  labels:
+    plugin: register-node
+spec:
+  nodeName: $node
+  containers:
+  - name: register-node
+    image: busybox
+    imagePullPolicy: IfNotPresent
+    command: ['sh', '-c']
+    args:
+    - |
+      USERNAME=\$(cat /etc/username/username);
+      PUBKEY=\$(cat /etc/rsa/id_rsa.pub);
+      mkdir -p /host/home/\$USERNAME/.ssh;
+      echo \$PUBKEY >> /host/home/\$USERNAME/.ssh/authorized_keys;
+    tty: true
+    stdin: true
+    stdinOnce: true
+    securityContext:
+      privileged: true
+    volumeMounts:
+    - name: host
+      mountPath: /host
+    - name: node-public
+      mountPath: /etc/rsa
+    - name: node-username
+      mountPath: /etc/username
+  volumes:
+  - name: host
+    hostPath:
+      path: /
+  - name: node-public
+    secret:
+        secretName: node-public
+  - name: node-username
+    secret:
+        secretName: node-username
+  hostNetwork: true
+  hostIPC: true
+  hostPID: true
+  restartPolicy: Never
+EOF
+)
+
+while :
+do
+    result=$(kubectl delete pod --field-selector=status.phase==Succeeded -n $ns -l plugin=register-node)
+    if [ "$result" == 'No resources found' ]; then
+        sleep 3
+    else
+        break
+    fi
+done
