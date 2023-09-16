e5c0d0fd3a85500d8a467d3d4f78d3f7c356718a
creaddiscans <creaddiscans@gmail.com>
2023-09-16 14:39:35 +0000

    localscaler update

--- terraform/autoscaler/main.tf
+++ terraform/autoscaler/main.tf
@@ -31,7 +31,7 @@ resource "kubernetes_deployment" "localscaler" {
         node_name    = "master"
         container {
           name  = "localscaler"
-          image = "creaddiscans/localscaler:0.21"
+          image = "creaddiscans/localscaler:0.28"
           env {
             name  = "PROMETHEUS"
             value = data.kubernetes_service.prometheus.spec.0.cluster_ip
