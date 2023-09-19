75c354a9e07ab6772b721577505c865b1e82de56
creaddiscans <creaddiscans@gmail.com>
2023-09-19 05:14:00 +0000

    폴더명 변경

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,57 +1,59 @@
-module "nginx_ingress" {
-  source       = "./nginx_ingress"
+module "nginx" {
+  source       = "./nginx"
   external_ips = var.external_ips
   email        = var.email
 }
 
-module "prometheus_monitoring" {
-  source     = "./prometheus-monitoring"
-  domain     = var.domain
-  mode       = var.mode
-  depends_on = [module.nginx_ingress]
-}
-
-module "autoscaler" {
-  source     = "./autoscaler"
-  depends_on = [module.prometheus_monitoring]
-  domain     = var.domain
-  mode       = var.mode
-}
-
-module "nfs_provisioner" {
-  source   = "./nfs_provisioner"
+module "nfs" {
+  source   = "./nfs"
   nfs_ip   = var.nfs_ip
   nfs_path = var.nfs_path
-}
-
-module "minio_storage" {
-  source     = "./minio_storage"
-  domain     = var.domain
-  mode       = var.mode
-  username   = var.username
-  password   = var.password
-  depends_on = [module.nfs_provisioner]
-}
-
-module "cnpg" {
-  source     = "./cnpg"
-  username   = var.username
-  password   = var.password
-  depends_on = [module.minio_storage]
 }
 
 module "istio" {
   source = "./istio"
 }
 
-module "keycloak" {
-  source     = "./keycloak"
-  domain     = var.domain
-  mode       = var.mode
-  username   = var.username
-  password   = var.password
-  depends_on = [module.istio]
-}
+# module "keycloak" {
+#   source     = "./keycloak"
+#   domain     = var.domain
+#   mode       = var.mode
+#   username   = var.username
+#   password   = var.password
+#   depends_on = [module.istio]
+# }
+
+# module "prometheus_monitoring" {
+#   source     = "./prometheus-monitoring"
+#   domain     = var.domain
+#   mode       = var.mode
+#   depends_on = [module.nginx_ingress]
+# }
+
+# module "autoscaler" {
+#   source     = "./autoscaler"
+#   depends_on = [module.prometheus_monitoring]
+#   domain     = var.domain
+#   mode       = var.mode
+# }
+
+# module "minio_storage" {
+#   source     = "./minio_storage"
+#   domain     = var.domain
+#   mode       = var.mode
+#   username   = var.username
+#   password   = var.password
+#   depends_on = [module.nfs_provisioner]
+# }
+
+# module "cnpg" {
+#   source     = "./cnpg"
+#   username   = var.username
+#   password   = var.password
+#   depends_on = [module.minio_storage]
+# }
+
+
 
 
 # module "gitlab_devops" {
