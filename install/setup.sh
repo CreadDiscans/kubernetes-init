04349f6c357f04554fb133f183e9be831144c33b
creaddiscans <creaddiscans@gmail.com>
2023-10-05 06:00:51 +0000

    install script에 docker 설치 부분 추가

--- install/setup.sh
+++ install/setup.sh
@@ -32,6 +32,15 @@ sudo apt install -y nfs-common
 # wake on lan
 sudo apt install -y net-tools ethtool wakeonlan
 
+# docker 설치
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 # kubernetes 설치
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 br_netfilter
