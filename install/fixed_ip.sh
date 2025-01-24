33290b9db13a397c359929afdbf77a93600b5bb4
creaddiscans <creaddiscans@gmail.com>
2025-01-24 07:22:09 +0000

    fixed_ip 스트립트 24.04 에 맞춰 수정정

--- install/fixed_ip.sh
+++ install/fixed_ip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $# -ne 2 ]; then
-    echo "Usage: $0 IP_ADDRESS GATEWAY_IP" 
+    echo "Usage: $0 IP_ADDRESS GATEWAY_IP"
     exit -1
 fi
-sudo sed -i 's/dhcp4: true/dhcp4: false\n      addresses: ['$1'\/24]\n      routes:\n        - to: default\n          via: '$2'\n      nameservers:\n        addresses: [8.8.8.8, 8.8.4.4]/g' /etc/netplan/50-cloud-init.yaml
+sudo sed -i 's/dhcp4: true/dhcp4: false\n            addresses: ['$1'\/24]\n            routes:\n              - to: default\n                via: '$2'\n            nameservers:\n              addresses: [8.8.8.8, 8.8.4.4]/g' /etc/netplan/50-cloud-init.yaml
 sudo netplan apply
