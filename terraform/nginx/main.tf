8160e453a3506da2364bb520042248045d8fbc6f
creaddiscans <creaddiscans@gamil.com>
2025-02-27 09:42:34 +0000

    nginx config map 수정정

--- terraform/nginx/main.tf
+++ terraform/nginx/main.tf
@@ -8,6 +8,24 @@ resource "kubernetes_namespace" "ns" {
   }
 }
 
+resource "kubernetes_config_map" "cm" {
+  metadata {
+    name      = "ingress-nginx-controller"
+    namespace = kubernetes_namespace.ns.metadata.0.name
+    labels = {
+      "app.kubernetes.io/component" = "controller"
+      "app.kubernetes.io/instance"  = "ingress-nginx"
+      "app.kubernetes.io/name"      = "ingress-nginx"
+      "app.kubernetes.io/part-of"   = "ingress-nginx"
+      "app.kubernetes.io/version"   = "1.12.0"
+    }
+  }
+  data = {
+    "proxy-body-size"           = "100g"
+    "allow-snippet-annotations" = "true"
+  }
+}
+
 module "ingress_nginx" {
   source     = "../utils/apply"
   yaml       = "${path.module}/yaml/ingress-nginx-v1.12.0.yaml"
