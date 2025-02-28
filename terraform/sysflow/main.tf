835321a5862793c3b00f44d565913841ab14619e
creaddiscans <creaddiscans@gamil.com>
2025-02-28 06:42:47 +0000

    sysflow upgrade

--- terraform/sysflow/main.tf
+++ terraform/sysflow/main.tf
@@ -25,11 +25,6 @@ resource "random_password" "secret_key" {
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
-resource "random_password" "db_password" {
-  length           = 16
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
-}
 resource "kubernetes_secret" "secret" {
   metadata {
     name      = "sysflow-secret"
@@ -47,112 +42,6 @@ resource "kubernetes_secret" "secret" {
     KEYCLOAK_CLIENT_SECRET = module.oidc.auth.client_secret
     KEYCLOAK_USERNAME      = var.keycloak.username
     KEYCLOAK_PASSWORD      = var.keycloak.password
-    DB_ENGINE              = "django.db.backends.postgresql"
-    DB_NAME                = "sysflow"
-    DB_USER                = "sysflow"
-    DB_PASSWORD            = random_password.db_password.result
-    DB_HOST                = "postgres-service"
-    DB_PORT                = "5432"
-  }
-}
-
-resource "kubernetes_persistent_volume_claim" "postgres_pvc" {
-  metadata {
-    name      = "postgres-pvc"
-    namespace = kubernetes_namespace.ns.metadata.0.name
-  }
-  spec {
-    access_modes = ["ReadWriteOnce"]
-    resources {
-      requests = {
-        "storage" = "1Gi"
-      }
-    }
-  }
-}
-
-resource "kubernetes_deployment" "postgres" {
-  metadata {
-    name      = "postgres-deploy"
-    namespace = kubernetes_namespace.ns.metadata.0.name
-    labels = {
-      app = "postgres"
-    }
-  }
-  spec {
-    replicas = 1
-    selector {
-      match_labels = {
-        app = "postgres"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = "postgres"
-        }
-      }
-      spec {
-        container {
-          name  = "postgres"
-          image = "postgres:17"
-          port {
-            container_port = 5432
-          }
-          env {
-            name = "POSTGRES_DB"
-            value_from {
-              secret_key_ref {
-                name = kubernetes_secret.secret.metadata.0.name
-                key  = "DB_NAME"
-              }
-            }
-          }
-          env {
-            name = "POSTGRES_USER"
-            value_from {
-              secret_key_ref {
-                name = kubernetes_secret.secret.metadata.0.name
-                key  = "DB_USER"
-              }
-            }
-          }
-          env {
-            name = "POSTGRES_PASSWORD"
-            value_from {
-              secret_key_ref {
-                name = kubernetes_secret.secret.metadata.0.name
-                key  = "DB_PASSWORD"
-              }
-            }
-          }
-          volume_mount {
-            name       = "data"
-            mount_path = "/var/lib/postgresql/data"
-          }
-        }
-        volume {
-          name = "data"
-          persistent_volume_claim {
-            claim_name = kubernetes_persistent_volume_claim.postgres_pvc.metadata.0.name
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "postgres_service" {
-  metadata {
-    name      = "postgres-service"
-    namespace = kubernetes_namespace.ns.metadata.0.name
-  }
-  spec {
-    port {
-      port        = 5432
-      target_port = 5432
-    }
-    selector = kubernetes_deployment.postgres.metadata.0.labels
   }
 }
 
@@ -179,6 +68,15 @@ resource "kubernetes_cluster_role" "cluster_role" {
       "patch",
       "update",
       "watch",
+    ]
+  }
+  rule {
+    api_groups = ["networking.k8s.io"]
+    resources  = ["ingresses"]
+    verbs = [
+      "get",
+      "list",
+      "watch"
     ]
   }
 }
@@ -224,30 +122,24 @@ resource "kubernetes_deployment" "deploy" {
         service_account_name = kubernetes_service_account.sa.metadata.0.name
         container {
           name  = "sysflow"
-          image = "creaddiscans/sysflow:1.0.7"
+          image = "creaddiscans/sysflow:1.0.9"
           port {
             container_port = 80
           }
-          volume_mount {
-            name       = "secret"
-            mount_path = "/etc/secret/"
-            read_only  = true
+          env_from {
+            secret_ref {
+              name = kubernetes_secret.secret.metadata.0.name
+            }
           }
         }
         container {
           name    = "operator"
-          image   = "creaddiscans/sysflow:1.0.7"
+          image   = "creaddiscans/sysflow:1.0.9"
           command = ["bash", "-c", "python3 manage.py operator --settings=config.prod.settings"]
-          volume_mount {
-            name       = "secret"
-            mount_path = "/etc/secret/"
-            read_only  = true
-          }
-        }
-        volume {
-          name = "secret"
-          secret {
-            secret_name = kubernetes_secret.secret.metadata.0.name
+          env_from {
+            secret_ref {
+              name = kubernetes_secret.secret.metadata.0.name
+            }
           }
         }
       }
